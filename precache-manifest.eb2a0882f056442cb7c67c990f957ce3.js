6873e874e22ea0385d5c40511ad3fb3579a8c323
Andran1 <Andran1@users.noreply.github.com>
2020-08-25 18:19:29 +0400

    Updates

---
+++ precache-manifest.eb2a0882f056442cb7c67c990f957ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb8391d198e087abf309c0773210417",
+    "url": "/webmagnet/index.html"
+  },
+  {
+    "revision": "60ebae84d6ecfbf41edd",
+    "url": "/webmagnet/static/css/2.4be015c5.chunk.css"
+  },
+  {
+    "revision": "6c00dab29115abe5e8c3",
+    "url": "/webmagnet/static/css/main.9ebb56ca.chunk.css"
+  },
+  {
+    "revision": "60ebae84d6ecfbf41edd",
+    "url": "/webmagnet/static/js/2.406b567e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/webmagnet/static/js/2.406b567e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c00dab29115abe5e8c3",
+    "url": "/webmagnet/static/js/main.dda94e4f.chunk.js"
+  },
+  {
+    "revision": "d49ed24bd61195f1f2a4",
+    "url": "/webmagnet/static/js/runtime-main.d1eb596a.js"
+  }
+]);
